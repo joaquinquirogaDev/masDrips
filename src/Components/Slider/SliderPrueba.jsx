4e17f12c342278952899d63b60539ca6d5eacb65
Joaquin Quiroga <joaquinquiroga22@gmail.com>
2022-11-20 12:33:18 -0300

    card

--- src/Components/Slider/SliderPrueba.jsx
+++ src/Components/Slider/SliderPrueba.jsx
@@ -1,20 +1,12 @@
 import React, { useState } from 'react'
 import Slider from 'react-slick'
 import './SliderPrueba.css'
-import Footer from '../Footer/Footer.jsx'
 import "slick-carousel/slick/slick-theme.css"
 import "slick-carousel/slick/slick.css"
 import Tratamientos from '../../Tratamientos'
-import stich from '../Imagenes/stich.png'
-import bart from '../Imagenes/bart.png'
-import pokemon from '../Imagenes/perro.jpeg'
-import dona from '../Imagenes/dona.png'
-import homero from '../Imagenes/homero.png'
-import mario from '../Imagenes/mario.jpeg'
-const imagenes = [stich, bart, pokemon, mario, homero, dona]
+
 
 export default function SliderPrueba() {
-    console.log(imagenes)
     const [slideIndex, setSlideIndex] = useState(0)
 
     const settings = {
@@ -22,7 +14,7 @@ export default function SliderPrueba() {
         infinite: true,
         speed: 500,
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         initialSlide: 0,
         responsive: [
             {
@@ -37,9 +29,9 @@ export default function SliderPrueba() {
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    initialSlide: 2,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    initialSlide: 1,
                 },
             },
             {
@@ -51,7 +43,9 @@ export default function SliderPrueba() {
             },
         ],
         beforeChange: (current, next) => setSlideIndex(next),
-        centerMode: true
+        centerMode: true,
+        variableWidth: true
+
     };
 
 
@@ -59,24 +53,28 @@ export default function SliderPrueba() {
         <div class="slider">
             <Slider {...settings}>
                 {Tratamientos?.map((item, index) => (
-                    <div class={index === slideIndex ? "card card-active" : "card" }>
+                    <div class={index === slideIndex ? "card card-active" : "card"}>
                         <div class="card-info">
-                            <p class="text-title">{item.Titulo} </p>
-                            <p class="text-body">{item.descripcion}</p>
+                            <p class={index === slideIndex ? "text-title text-title-active" : "text-title"}>{item.Titulo} </p>
+                            <p class={index === slideIndex ? "text-body text-body-active" : "text-body"}>{item.descripcion}</p>
                         </div>
                         <div class="card-footer">
                             <span class="text-title">{item.precio}</span>
-                            {/* <div class="card-button">
-                                <svg class="svg-icon" viewBox="0 0 20 20">
-                                    <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
-                                    <path d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z"></path>
-                                    <path d="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z"></path>
-                                </svg>
-                            </div> */}
-                        </div></div>
+
+                        </div>
+                        <div>
+
+                        <button class={index === slideIndex ? "button button-active" : "button"}>
+                            Reservar
+                        </button>
+                        <button>
+                            Ver mas
+                        </button>
+                        </div>
+
+                    </div>
                 ))}
             </Slider>
-            {/* <Footer/> */}
         </div>
     )
 }
